071f532655d1631f04b14449d173453295963637
Felix-Pi <49276377+Felix-Pi@users.noreply.github.com>
2021-06-14 14:48:31 +0200

    Initial Commit

---
+++ Berliner-Badestellen/Berliner_BadestellenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Berliner_BadestellenApp.swift
+//  Berliner-Badestellen
+//
+//  Created by Felix Pieschka on 14.06.21.
+//
+
+import SwiftUI
+
+@main
+struct Berliner_BadestellenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Berliner-Badestellen/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Berliner-Badestellen
+//
+//  Created by Felix Pieschka on 14.06.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
